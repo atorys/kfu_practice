1a2da719109501bdc595fb04266aada5d169e979
atorys <atory@student.21-school.ru>
2021-09-27 21:44:04 +0300

    1st homework c++

---
+++ hw1/N1.cpp
@@ -0,0 +1,64 @@
+//
+// Created by atory on 25.09.2021.
+//
+#include <iostream>
+
+/**
+ * (без циклов)
+ * определить является ли число трехзначным
+ */
+int	main(void)
+{
+	setlocale(LC_ALL, "Russian");
+	int digit;
+	int len = 0;
+
+	std::cout << "Ведите число : ";
+	std::cin >> digit;
+
+	if (digit % 10)
+		len++;
+
+	digit /= 10;
+	if (digit % 10)
+		len++;
+
+	digit /= 10;
+	if (digit % 10)
+		len++;
+
+	digit /= 10;
+	if (digit % 10)
+		len++;
+
+	if (len == 3)
+		std::cout << "ОК" << std::endl;
+	else
+		std::cout << "KO : число не трехзначное !!!" << std::endl;
+	return (0);
+}
+
+/**
+ * (c использованием цикла)
+ * определить является ли число трехзначным
+ */
+//int	main(void)
+//{
+//	setlocale(LC_ALL, "Russian");
+//	int digit;
+//	int len = 0;
+//
+//	std::cout << "Ведите число : ";
+//	std::cin >> digit;
+//
+//	while (digit % 10 != 0)
+//	{
+//		len++;
+//		digit /= 10;
+//	}
+//	if (len == 3)
+//		std::cout << "ОК" << std::endl;
+//	else
+//		std::cout << "KO : число не трехзначное !!!" << std::endl;
+//	return (0);
+//}
